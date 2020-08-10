2192f47b26e59694354394b2f80ef50b9fc7c1cb
Thomason <super_yangtao@126.com>
2020-08-10 18:23:56 +0800

    完善注释

--- framework-r2dbc/src/main/java/org/loed/framework/r2dbc/inspector/dialect/DatabaseDialect.java
+++ framework-r2dbc/src/main/java/org/loed/framework/r2dbc/inspector/dialect/DatabaseDialect.java
@@ -12,12 +12,39 @@ import reactor.core.publisher.Mono;
  * @since 2020/8/8 11:57 上午
  */
 public interface DatabaseDialect {
-	Mono<Table> getTable(Connection connection,@Nullable String catalog,@Nullable String schema, @NonNull String tableName);
+	/**
+	 * 根据 catalog schema tableName 查询表结构定义
+	 *
+	 * @param connection r2dbc 连接
+	 * @param catalog    catalog
+	 * @param schema     schema
+	 * @param tableName  tableName
+	 * @return 表定义，如果不存在，则返回空
+	 */
+	Mono<Table> getTable(Connection connection, @Nullable String catalog, @Nullable String schema, @NonNull String tableName);
 
+	/**
+	 * 根据表定义构造创建表的sql
+	 *
+	 * @param table 表定义
+	 * @return 建表sql
+	 */
 	String createTable(org.loed.framework.common.orm.Table table);
 
+	/**
+	 * 根据列定义，构建add column 语句
+	 *
+	 * @param column 列定义
+	 * @return add column 语句
+	 */
 	String addColumn(org.loed.framework.common.orm.Column column);
 
+	/**
+	 * 根据索引定义，构建添加索引的sql语句
+	 *
+	 * @param index 索引定义
+	 * @return 增加索引的sql语句
+	 */
 	String addIndex(org.loed.framework.common.orm.Index index);
 
 	/**
@@ -34,7 +61,13 @@ public interface DatabaseDialect {
 	 */
 	String quote();
 
-	default String wrap(String raw) {
+	/**
+	 * 包装表名称和列名称
+	 *
+	 * @param raw 原始的表名称或者列名称
+	 * @return 包装后的表名或者列名
+	 */
+	default String wrap(@NonNull String raw) {
 		if (isQuote()) {
 			return quote() + raw + quote();
 		}
