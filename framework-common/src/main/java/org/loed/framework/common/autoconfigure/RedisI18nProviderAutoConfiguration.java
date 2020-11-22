a81e89cba74d2689abc0133574c3c805d563f707
Thomason <super_yangtao@126.com>
2020-11-22 15:40:35 +0800

    修改redis启动类

--- framework-common/src/main/java/org/loed/framework/common/autoconfigure/RedisI18nProviderAutoConfiguration.java
+++ framework-common/src/main/java/org/loed/framework/common/autoconfigure/RedisI18nProviderAutoConfiguration.java
@@ -2,12 +2,13 @@ package org.loed.framework.common.autoconfigure;
 
 import org.loed.framework.common.web.mvc.i18n.I18nProvider;
 import org.loed.framework.common.web.mvc.i18n.RedisI18nProvider;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.core.RedisOperations;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
 
 /**
@@ -16,10 +17,11 @@ import org.springframework.data.redis.core.StringRedisTemplate;
  * @since 2020/8/17 10:39 上午
  */
 @ConditionalOnClass(RedisOperations.class)
+@AutoConfigureAfter(RedisAutoConfiguration.class)
 @Configuration
 public class RedisI18nProviderAutoConfiguration {
 	@Bean
-	@ConditionalOnBean(RedisTemplate.class)
+	@ConditionalOnBean(StringRedisTemplate.class)
 	public I18nProvider redisI18nProvider(StringRedisTemplate redisTemplate) {
 		RedisI18nProvider redisI18nProvider = new RedisI18nProvider();
 		redisI18nProvider.setRedisTemplate(redisTemplate);
